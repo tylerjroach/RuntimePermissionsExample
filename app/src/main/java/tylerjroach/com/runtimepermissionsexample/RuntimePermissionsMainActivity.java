81415a7fe39eb0fd9278b8cb6a75b884d13f1baf
Tyler <tylerjroach@gmail.com>
2016-09-01 11:48:28 -0400

    Add permission status.

--- app/src/main/java/tylerjroach/com/runtimepermissionsexample/RuntimePermissionsMainActivity.java
+++ app/src/main/java/tylerjroach/com/runtimepermissionsexample/RuntimePermissionsMainActivity.java
@@ -7,12 +7,14 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class RuntimePermissionsMainActivity extends AppCompatActivity implements
     CameraPermissionsDialogFragment.CameraPermissionsGrantedCallback {
 
   FloatingActionButton fab;
+  TextView status;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -24,16 +26,32 @@ public class RuntimePermissionsMainActivity extends AppCompatActivity implements
         navigateToCaptureFragment();
       }
     });
+
+    status = (TextView) findViewById(R.id.status);
+  }
+
+  @Override protected void onResume() {
+    super.onResume();
+
+    if (isPermissionGranted()) {
+      status.setText("All permissions granted. Ready to open Camera");
+    } else {
+      status.setText("Permissions Needed");
+    }
   }
 
   @Override public void navigateToCaptureFragment() {
-    if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
-    ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED &&
-    ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-      Toast.makeText(this, "Ready to open camera", Toast.LENGTH_LONG).show();
+    if (isPermissionGranted()) {
+      Toast.makeText(this, "Opening Camera!", Toast.LENGTH_LONG).show();
     } else {
       CameraPermissionsDialogFragment.newInstance().show(getSupportFragmentManager(), CameraPermissionsDialogFragment.class.getName());
     }
+  }
+
+  private boolean isPermissionGranted() {
+    return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
+        ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED &&
+        ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
   }
 
 }
